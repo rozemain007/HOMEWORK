bcfde03c776482068573c8b05db6a5c98c31e37f
rozemain007 <ivanovanastya1896@gmail.com>
2025-10-21 20:08:49 +0500

    Update lesson05_task2.py

--- lesson05_task2.py
+++ lesson05_task2.py
@@ -12,6 +12,7 @@ driver.get("http://uitestingplayground.com/dynamicid")
 blue_button_locator = '[class="btn btn-primary"]'
 searh_blue_button = driver.find_element(By.CSS_SELECTOR, blue_button_locator)
 print("Я нашла голубую кнопку c динамическим айди!!!")
-searh_blue_button.click
+searh_blue_button.click()
 print("Я кликнула на голубую кнопку с динамическим айди!")
+
 sleep(10)
